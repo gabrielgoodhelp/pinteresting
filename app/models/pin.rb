3dc8bc7ebd42833ac8cfe738facb458079d4bd97
Gabriele <gabriele.buonaiuto@gmail.com>
2014-07-06 16:27:02 +0200

    still trying to sort the heroku upload out

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,14 +4,6 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, 
 	:styles => { :medium => "300x300>", :thumb => "100x100>" }, 
 	:default_url => "/images/:style/missing.png",
-	:storage => :s3,
-    :s3_credentials => {
-    	:bucket => ENV['S3_BUCKET_NAME'],
-    	:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    	:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  		}
-  		:s3_permissions => 'private',
-        :url => ":s3_domain_url"
 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
