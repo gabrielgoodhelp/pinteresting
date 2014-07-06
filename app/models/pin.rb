39cb1cedc4a762097cfee0ea439ff47984978996
Gabriele <gabriele.buonaiuto@gmail.com>
2014-07-06 21:25:01 +0200

    bad again

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,8 +2,8 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	
 	has_attached_file :image, 
-	:styles => {t_url => "/images/:style/missing.png",
-	:stor :medium => "300x300>", :thumb => "100x100>" }
+	:styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+	:default_url => "/images/:style/missing.png"
 
 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
