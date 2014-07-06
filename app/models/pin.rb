87c88bfc0f7cdf06af193fc515ef8a2a3f247e91
Gabriele <gabriele.buonaiuto@gmail.com>
2014-07-06 21:20:05 +0200

    no improvements

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,8 +5,7 @@ class Pin < ActiveRecord::Base
 	:styles => { :medium => "300x300>", :thumb => "100x100>" }, 
 	:default_url => "/images/:style/missing.png",
 	:storage => :s3,
-    :bucket => 'photosharingfirstproject',
-    :s3_credentials => S3_CREDENTIALS
+    :bucket => 'photosharingfirstproject'
 
 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
