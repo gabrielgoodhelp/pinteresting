ebf513f23aa7ec729ce946bc46d532216dac5873
Gabriele <gabriele.buonaiuto@gmail.com>
2014-07-06 21:39:06 +0200

    maybe again

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,16 @@ class Pin < ActiveRecord::Base
 	
 	has_attached_file :image, 
 	:styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-	:default_url => "/images/:style/missing.png"
+	:default_url => "/images/:style/missing.png",
+
+
+    :storage => :s3,
+    :s3_credentials => "#{Rails.root}/config/amazon_s3.yml",
+    :s3_protocol => "https",
+    :path => ":class/:id/:basename_:style.:extension",
+    :bucket => "photosharingfirstproject",
+    :url  => ":s3_eu_url"
+end
 
 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
