4f4162e9be0e187c2bddeb9262c680a4aadcd2e3
Gabriele <gabriele.buonaiuto@gmail.com>
2014-07-06 22:14:09 +0200

    again

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,8 +5,5 @@ class Pin < ActiveRecord::Base
 	:styles => { :medium => "300x300>", :thumb => "100x100>" }, 
 	:default_url => "/images/:style/missing.png"
 
-end
-
-
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
