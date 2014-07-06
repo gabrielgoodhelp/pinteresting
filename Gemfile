754ea12583f3810d5363200d0f4075d9d71296bc
Gabriele <gabriele.buonaiuto@gmail.com>
2014-07-06 12:14:22 +0200

    Added AWS S3 to gems and solved Nokogiri/PG issues

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'devise', '~> 3.2.4'
 
 gem 'paperclip', github: 'thoughtbot/paperclip'
 
+gem 'aws-sdk', '~> 1.46.0'
+
 
 group :development, :test do
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,16 @@ Rails.application.configure do
   # Required for Heroku
   # -------------- Note to set this to the actual host -----------
   config.action_mailer.default_url_options = { host: 'gabriele-pinteresting.herokuapp.com' }
+
+  # Sets Paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
   
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
