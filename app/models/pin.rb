3d78b4c412afbdacaa96c50b01fd747459baf401
Gabriele <gabriele.buonaiuto@gmail.com>
2014-07-06 16:28:54 +0200

    in deep trouble with heroku uploads

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,7 @@ class Pin < ActiveRecord::Base
 	
 	has_attached_file :image, 
 	:styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-	:default_url => "/images/:style/missing.png",
+	:default_url => "/images/:style/missing.png"
 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
