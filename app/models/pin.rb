578c524ea54f051787a64be863eb20a857684ae6
Gabriele <gabriele.buonaiuto@gmail.com>
2014-07-06 22:10:22 +0200

    S3 moved to US standard

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,15 +3,8 @@ class Pin < ActiveRecord::Base
 	
 	has_attached_file :image, 
 	:styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-	:default_url => "/images/:style/missing.png",
+	:default_url => "/images/:style/missing.png"
 
-
-    :storage => :s3,
-    :s3_credentials => "#{Rails.root}/config/amazon_s3.yml",
-    :s3_protocol => "https",
-    :path => ":class/:id/:basename_:style.:extension",
-    :bucket => "photosharingfirstproject",
-    :url  => ":s3_eu_url"
 end
 
 
