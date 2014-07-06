16d8302f362103b17d4fce9bd1e4bd68af7e24b7
Gabriele <gabriele.buonaiuto@gmail.com>
2014-07-06 21:22:28 +0200

    maybe

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,10 +2,8 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	
 	has_attached_file :image, 
-	:styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-	:default_url => "/images/:style/missing.png",
-	:storage => :s3,
-    :bucket => 'photosharingfirstproject'
+	:styles => {t_url => "/images/:style/missing.png",
+	:stor :medium => "300x300>", :thumb => "100x100>" }
 
 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
